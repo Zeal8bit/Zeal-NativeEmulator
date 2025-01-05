b61b6acc43545af1c91af3f2a69fa6d4f9e7c062
Zeal8bit <contact@zeal8bit.com>
2025-01-05 22:34:22 +0800

    hw/z80: update Z80 header to reflect z80_step's new behavior

--- include/hw/z80.h
+++ include/hw/z80.h
@@ -43,7 +43,7 @@ struct z80 {
 };
 
 void z80_init(z80* const z);
-void z80_step(z80* const z);
+int  z80_step(z80* const z);
 void z80_debug_output(z80* const z);
 void z80_gen_nmi(z80* const z);
 void z80_gen_int(z80* const z, uint8_t data);
