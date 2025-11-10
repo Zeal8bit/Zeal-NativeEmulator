f4be4682faf85a4563cd69641b657426077d24a2
Zeal8bit <contact@zeal8bit.com>
2025-11-10 20:11:13 +0800

    log: add missing newline to perror logging

--- include/utils/log.h
+++ include/utils/log.h
@@ -15,4 +15,4 @@
 #define log_printf(...)         printf(__VA_ARGS__)
 #define log_err_printf(...)     fprintf(stderr, __VA_ARGS__)
 #define log_perror(fmt, ...) \
-    fprintf(stderr, fmt ": %s", ##__VA_ARGS__, strerror(errno))
+    fprintf(stderr, fmt ": %s\n", ##__VA_ARGS__, strerror(errno))
