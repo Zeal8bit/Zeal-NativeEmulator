5a41d29be5e2cf46f1c28348f687a8562d6854d9
David Higgins <zoul0813@me.com>
2026-06-27 09:15:24 -0400

    add flag to disable auto attached snes mouse, some platforms just don't need this extra code running disable mouse auto-attach on rpi's

--- hw/userport/snes_adapter/snes_adapter.c
+++ hw/userport/snes_adapter/snes_adapter.c
@@ -165,7 +165,9 @@ int snes_adapter_init(snes_adapter_t* snes_adapter, pio_t* pio)
     snes_mouse_init(&snes_adapter->mouse);
     snes_adapter->mouse.machine = pio->machine;
     snes_adapter_listen(snes_adapter);
+#ifdef CONFIG_AUTO_ATTACH_SNES_MOUSE
     snes_adapter_set_mouse_port(snes_adapter, SNES_MOUSE_DEFAULT_PORT);
+#endif
 
     snes_adapter_attach_available_controllers(snes_adapter);
 
